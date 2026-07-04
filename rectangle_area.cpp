b177b7e7ed7f297ecdbde9ae01356f7bd28c0744
tiktiki <itzpomodoro79@gmail.com>
2026-07-04 13:00:11 +0600

    area calculator

---
+++ rectangle_area.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> //includes all the standard libraries use in cp
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int l, b;
+        cin >> l >> b;
+        cout << l * 1LL * b << endl;
+    }
+
+    return 0;
+}
