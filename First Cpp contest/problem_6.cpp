f687b5c49d9fe54dc44c147db43e52c9905848d3
tiktiki <itzpomodoro79@gmail.com>
2026-07-16 22:55:19 +0600

    add problem 6 solving file

--- First Cpp contest/problem_6.cpp
+++ First Cpp contest/problem_6.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int t;
+    cin >> t;
+    while(t--){
+        int N;
+        cin >> N;
+        vector<int> v = {1};
 
-
+        for(int i=0; i < N; i++){
+            for(int j = 0; j < i+1; j++){
+                if(j <= i/2){
+                    cout << v[j] << " ";
+                }else{
+                    cout << v[i-j] << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
 }
