d0c1cefd4756d4105bb9812e8c4b35062ed3b67a
tiktiki <itzpomodoro79@gmail.com>
2026-07-18 22:56:56 +0600

    rename folder of My solution

---
+++ First Cpp contest/My solution/problem_7.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string line;
+    cout << "Enter a string: ";
+    cin >> line;
+    
+    long long total = 0;
+
+    for(char c: line)
+        int value = int(c);
+
+
+    return 0;
+}
