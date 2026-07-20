140510cf028e8e32a17cde02ecdc62d3995079fd
tiktiki <itzpomodoro79@gmail.com>
2026-07-20 21:00:59 +0600

    add most optimal solution of 2D array sum

--- prefix sum/twoD_arr.cpp
+++ prefix sum/twoD_arr.cpp
@@ -15,12 +15,14 @@ constraints
 using namespace std;
 const int N = 1e3+10;
 int ar[N][N];
+long long pf[N][N];
 int main(){
     int n;
     cin >> n;
     for(int i = 1; i <= n; ++i){
         for(int j = 1; j <= n; ++j){
             cin >> ar[i][j];
+            pf[i][j] = (ar[i][j] + ar[i-1][j] + ar[i][j-1]) - ar[i-1][j-1]; 
         }
     }
     int q;
@@ -28,12 +30,8 @@ int main(){
     while(q--){
         int a, b, c, d;
         cin >> a >> b >> c >> d;
-        long long sum = 0;
-        for(int i = a; i <= c; ++i){
-            for(int j = b; j <= d; ++j){
-                sum += ar[i][j];
-            }
-        }
+        cout << pf[c][d] - pf[a-1][d] - pf[c][b-1] + pf[a-1][b-1] << endl;
+        
     }
 
 
