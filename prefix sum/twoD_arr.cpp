dd50dcb8a5c067d5fa575bc5a8c00db26d4c1583
tiktiki <itzpomodoro79@gmail.com>
2026-07-20 20:28:17 +0600

    add question of 2D array prefix sum problem

---
+++ prefix sum/twoD_arr.cpp
@@ -0,0 +1,19 @@
+/*
+Given @D array a of N*N integers. Given Q wuaeries
+and in each query given a, b, c and d. print sum
+of square represented by (a, b) as top left point 
+and (c, d) as top bottom right point
+
+constraints 
+1 <= N <= 10^3
+1 <= a[i][j] <= 10^9
+1 <= Q <= 10^5
+1 <= a, b, c, d <= N
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    return 0;
+}
