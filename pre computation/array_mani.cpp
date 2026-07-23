5473008c267a9494b1cfc4388200586728538584
tiktiki <itzpomodoro79@gmail.com>
2026-07-23 19:16:09 +0600

    optimal solution of git menipulation

--- pre computation/array_mani.cpp
+++ pre computation/array_mani.cpp
@@ -4,15 +4,18 @@ const int N = 1e7+10;
 long long int ar[N];
 
 int main(){
-    
+
     int n, m;
     cin >> n >> m;
     while(m--){
         int a, b, d;
         cin >> a >> b >> d;
-        for(int i = a; i <= b; ++i){
-            ar[i] += d;
-        }
+        ar[a] += d;
+        ar[b+1] -= d;
+
+    }
+    for(int i = 1; i <= n; ++i){
+        ar[i] += ar[i-1];
     }
 
     long long mx = -1;
