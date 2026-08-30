00e3fd2686a8c59d3a6031a23abe04ec46591f70
its-Tiktiki <itzpomodoro79@gmail.com>
2026-08-30 21:44:56 +0600

    update single number_1 bitmenipulation file

--- others practice/single_number1_bitmeni.cpp
+++ others practice/single_number1_bitmeni.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+
     vector<int> nums = {1, 2, 1, 2, 3};
 
     int ans = 0;
     for(int n : nums){
         ans ^= n;
     }
-    cout << ans << endl;
+    cout << "Answer: " << ans << endl;
 
     return 0;
 }
